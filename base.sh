d83e4e94c343dffa4c4b8921f5ceaf6626ffad1b
Afonso <joaquimafonsocunha@gmail.com>
2022-07-22 00:55:28 +0100

    Started working on the base (Still not tested)

--- base.sh
+++ base.sh
@@ -1,5 +1,50 @@
 #!/bin/env bash
 
-# Declare variables
-# Start install
+## WARNING: Still not tested ##
+
+### Post mount install ###
+
+#############
+# Variables #
+#############
+
+# User name
+name="name"
+
+# User passwd
+user_passwd="passwd"
+
+# Root passwd (Default is equal to user passwd)
+root_passwd=$user_passwd
+
+# Hostname
+hostname="hostname"
+
+#################
+# Start Install #
+#################
+
+## To find you region and city run timedatectl list-timezones and the format is (Region/City)
+ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
+hwclock --systohc
+
+sed -i "s/#en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
+# If you need more locales add other seds
+# example:
+sed -i "s/#en_GB.UTF-8/en_GB.UTF-8/g" /etc/locale.gen
+
+locale-gen
+
+# Set the language of choice (needed to be uncommented on locale.gen)
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# In case you need to change the keyboard layout
+# echo "KEYMAP=de-latin1" >> /etc/vconsole.conf
+
+echo $hostname >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+echo "127.0.1.1 $hostname" >> /etc/hosts
+
+echo "root:$root_passwd" | chpasswd
 
